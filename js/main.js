1800522912feeb241be6097843444fd5ab300ec5
Иван Чесноков <gr434_cheial@lab-57-9.tomtit-tomsk.ru>
2026-02-20 14:56:38 +0700

    3 commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 let app = new Vue({
     el: '#app',
-    data: {
-
-    },
+    data:
+        {
+            cards: []
+        },
     methods: {
-
+        addNoteCard() {
+            this.cards.push({
+                id: Date.now(),
+                title: 'Новая заметка',
+                items: [],
+                column: 1
+            });
+            this.saveCards();
+        },
+        saveCards() {
+            localStorage.setItem('notes-cards', JSON.stringify(this.cards));
+        },
+        loadCards() {
+            const savedCards = localStorage.getItem('notes-cards');
+            if (savedCards) {
+                this.cards = JSON.parse(savedCards);
+            }
+        }
+    },
+    computed: {
+        column1Cards() {
+            return this.cards.filter(card => card.column === 1);
+        }
+    },
+    mounted() {
+        this.loadCards();
     }
 })
